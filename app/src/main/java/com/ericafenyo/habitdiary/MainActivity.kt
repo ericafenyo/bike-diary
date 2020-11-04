c658bcbd6594abfe4b22a7c98be5780b264e1d34
eric <eric.afenyo@transway.fr>
2020-11-05 00:19:16 +0100

    add basic navigation structure

--- app/src/main/java/com/ericafenyo/habitdiary/MainActivity.kt
+++ app/src/main/java/com/ericafenyo/habitdiary/MainActivity.kt
@@ -1,11 +1,52 @@
 package com.ericafenyo.habitdiary
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.navigation.NavController
+import com.ericafenyo.habitdiary.databinding.ActivityMainBinding
+import com.ericafenyo.habitdiary.util.setupWithNavController
+import com.wada811.databinding.dataBinding
+
 
 class MainActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
-  }
+    private val binding: ActivityMainBinding by dataBinding()
+
+    private var currentNavController: LiveData<NavController>? = null
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null) {
+            setupBottomNavigationBar()
+        } // Else, need to wait for onRestoreInstanceState
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        // Now that BottomNavigationBar has restored its instance state
+        // and its selectedItemId, we can proceed with setting up the
+        // BottomNavigationBar with Navigation
+        setupBottomNavigationBar()
+    }
+
+    private fun setupBottomNavigationBar() {
+        val bottomNavigationView = binding.bottomNavigation
+        val navGraphIds = listOf(R.navigation.home, R.navigation.profile)
+
+        // Setup the bottom navigation view with a list of navigation graphs
+        val controller = bottomNavigationView.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_container,
+            intent = intent
+        )
+        currentNavController = controller
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: false
+    }
 }
