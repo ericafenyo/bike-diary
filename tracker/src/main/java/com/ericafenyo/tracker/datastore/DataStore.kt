3721780630056b2b7a3e2cbb32455c1a62d02475
ericafenyo <ericafenyo@yahoo.com>
2021-08-14 17:54:41 +0200

    Fix build error

--- tracker/src/main/java/com/ericafenyo/tracker/datastore/DataStore.kt
+++ tracker/src/main/java/com/ericafenyo/tracker/datastore/DataStore.kt
@@ -26,15 +26,19 @@
 package com.ericafenyo.tracker.datastore
 
 import android.content.Context
-import android.icu.text.AlphabeticIndex.Record
+
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
 @TypeConverters(Converters::class)
-@Database(entities = [Record::class], version = 1, exportSchema = false)
-internal abstract class DataStore : RoomDatabase() {
+@Database(
+  version = 1,
+  exportSchema = false,
+  entities = [Record::class]
+)
+abstract class DataStore : RoomDatabase() {
   abstract fun getRecords(): RecordDao
 
   companion object {
