cc0543651aac193799b6bd513901f2c409146583
ericafenyo <ericafenyo@yahoo.com>
2022-08-06 00:57:33 +0200

    feat: :construction: Add image carousel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -99,6 +99,7 @@ dependencies {
   implementation(projects.tracker)
   implementation(projects.libs.storage)
   implementation(projects.libs.icons)
+  implementation(projects.libs.pager)
 
   testImplementation(libs.junit)
   androidTestImplementation(libs.androidx.junitExt)
@@ -163,11 +164,11 @@ dependencies {
 
 //   debugImplementation(libs.leakCanary)
 
-  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
   
   implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 
-  implementation(libs.accompanist.pager)
+//  implementation(libs.accompanist.pager)
 }
 
 hilt {
--- app/src/main/java/com/ericafenyo/bikediary/ui/screens/adventure/details/AdventureDetails.kt
+++ app/src/main/java/com/ericafenyo/bikediary/ui/screens/adventure/details/AdventureDetails.kt
@@ -33,6 +33,10 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -42,8 +46,12 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
@@ -52,6 +60,9 @@ import com.ericafenyo.bikediary.theme.AppTheme
 import com.ericafenyo.bikediary.theme.headlineSmall
 import com.ericafenyo.bikediary.ui.screens.adventure.details.AdventureDetailsUiState.Loading
 import com.ericafenyo.bikediary.ui.screens.adventure.details.AdventureDetailsUiState.Success
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.HorizontalPager
+import com.google.accompanist.pager.rememberPagerState
 import java.time.Duration
 
 @Composable
@@ -100,14 +111,7 @@ fun AdventureDetails(
             .background(color = MaterialTheme.colors.surface)
         ) {
           Column {
-            AsyncImage(
-              modifier = Modifier
-                .fillMaxWidth()
-                .aspectRatio(1F),
-              model = "https://d2ucnymlzowcof.cloudfront.net/${adventure.image}",
-              contentDescription = null,
-              contentScale = ContentScale.Crop
-            )
+            ImageCarousel(imageUrl = adventure.image)
             Spacer(modifier = Modifier.height(16.dp))
             Text(
               text = adventure.title,
@@ -126,4 +130,60 @@ fun AdventureDetails(
       }
     }
   }
+}
+
+@OptIn(ExperimentalPagerApi::class)
+@Composable
+fun ImageCarousel(
+  modifier: Modifier = Modifier,
+  imageUrl: String,
+) {
+  Box(modifier = modifier) {
+    val pageState = rememberPagerState()
+
+    HorizontalPager(count = 10, state = pageState) {
+      AsyncImage(
+        modifier = Modifier
+          .fillMaxWidth()
+          .aspectRatio(1F),
+        model = "https://d2ucnymlzowcof.cloudfront.net/${imageUrl}",
+        contentDescription = null,
+        contentScale = ContentScale.Crop
+      )
+    }
+
+    Row(
+      modifier = Modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(Color(0xFF2B2B2B))
+        .padding(6.dp)
+        .align(Alignment.BottomEnd),
+      verticalAlignment = Alignment.CenterVertically
+    ) {
+      Icon(
+        modifier = Modifier.size(16.dp),
+        painter = Icons.Camera,
+        tint = Color.White,
+        contentDescription = null,
+      )
+      Spacer(modifier = Modifier.width(4.dp))
+      Text(
+        text = "${pageState.currentPage + 1}/${pageState.pageCount}",
+        color = Color.White
+      )
+    }
+  }
+}
+
+
+@Preview
+@Composable
+fun PreviewCarousel() {
+  ImageCarousel(
+    modifier = Modifier
+      .fillMaxWidth()
+      .background(Color.White),
+    imageUrl = ""
+  )
 }
